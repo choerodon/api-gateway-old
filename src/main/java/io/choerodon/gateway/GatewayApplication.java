0e79d092aa92b403d4ae132d5e36152ba730488c
flyleft <jcalaz@163.com>
2019-02-27 15:16:32 +0800

    [IMP] 修改deployment.yaml中健康检查探针为/actuator/health

--- src/main/java/io/choerodon/gateway/GatewayApplication.java
+++ src/main/java/io/choerodon/gateway/GatewayApplication.java
@@ -2,6 +2,7 @@ package io.choerodon.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.retry.annotation.EnableRetry;
@@ -14,7 +15,7 @@ import org.springframework.retry.annotation.EnableRetry;
 @EnableRetry
 @EnableZuulProxy
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class GatewayApplication {
 
     public static void main(String[] args) {
