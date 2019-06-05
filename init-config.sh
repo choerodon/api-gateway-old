74f671756575cc789e793ef9c11c82f80ad534ce
superlee <superleader8@gmail.com>
2019-06-05 13:57:42 +0800

    [UPD] 修改本地初始化配置脚本

--- init-config.sh
+++ init-config.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-MAVEN_LOCAL_REPO=$(mvn help:evaluate -Dexpression=settings.localRepository | grep -v '\[INFO\]')
+MAVEN_LOCAL_REPO=$(cd / && mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout)
 TOOL_GROUP_ID=io.choerodon
 TOOL_ARTIFACT_ID=choerodon-tool-config
-TOOL_VERSION=0.11.0.RELEASE
+TOOL_VERSION=${1:-0.11.0.RELEASE}
 TOOL_JAR_PATH=${MAVEN_LOCAL_REPO}/${TOOL_GROUP_ID/\./\/}/${TOOL_ARTIFACT_ID}/${TOOL_VERSION}/${TOOL_ARTIFACT_ID}-${TOOL_VERSION}.jar
 mvn org.apache.maven.plugins:maven-dependency-plugin:get \
- -Dartifact=${TOOL_GROUP_ID}:${TOOL_ARTIFACT_ID}:${TOOL_VERSION} \
- -Dtransitive=false
+  -Dartifact=${TOOL_GROUP_ID}:${TOOL_ARTIFACT_ID}:${TOOL_VERSION} \
+  -Dtransitive=false
+
+mvn clean install
 
 java -Dspring.datasource.url="jdbc:mysql://localhost/manager_service?useUnicode=true&characterEncoding=utf-8&useSSL=false" \
  -Dspring.datasource.username=root \
